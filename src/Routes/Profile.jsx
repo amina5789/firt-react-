5b6d2b25fab02adcb1168ccad65c0808a02530ab
amina5789 <merzamedovaa@gmail.com>
2025-03-05 12:57:55 +0300

    useReducer

--- src/Routes/Profile.jsx
+++ src/Routes/Profile.jsx
@@ -1,6 +1,45 @@
-export const Profile =()=>{
-    return <div>
-        <h4>Настройки</h4>
-        <h4>Личные данные </h4>
+// export const Profile =()=>{
+//     return <div>
+//         <h4>Настройки</h4>
+//         <h4>Личные данные </h4>
+//     </div>
+// }
+
+import { useReducer } from "react";
+
+const initialState = { count: 0, history: [0] };
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "increment":
+      return {
+        count: state.count + 1,
+        history: [...state.history, state.count + 1],
+      };
+    case "decrement":
+      return {
+        count: state.count - 1,
+        history: [...state.history, state.count - 1],
+      };
+    default:
+      return state;
+  }
+}
+
+export function  Profile() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  return (
+    <div >
+      <p>count: {state.count}</p>
+      <button onClick={() => dispatch({ type: "increment" })}>+</button>
+      <button onClick={() => dispatch({ type: "decrement" })}>-</button>
+      <h2>История:</h2>
+      <ul >
+        {state.history.map((val, index) => (
+          <li key={index}>{val}</li>
+        ))}
+      </ul>
     </div>
+  );
 }
